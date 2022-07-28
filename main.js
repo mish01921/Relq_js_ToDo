11e0bd5fb353e1fc5aa85e0dcf02e0bfc7a3d651
mish01921 <misho191524@gmail.com>
2022-07-28 17:23:02 +0400

    add space

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ let addMessage = document.getElementById("message");
 let button = document.getElementById("add");
 
 button.addEventListener ("click", function(){
+
+    
     console.log(addMessage.value);
 })
