1a2e7aec9e20c4a90228b60062b2b0b8fe085461
mish01921 <misho191524@gmail.com>
2022-07-28 17:19:01 +0400

    avelacri js

---
+++ main.js
@@ -0,0 +1,6 @@
+let addMessage = document.getElementById("message");
+let button = document.getElementById("add");
+
+button.addEventListener ("click", function(){
+    console.log(addMessage.value);
+})
